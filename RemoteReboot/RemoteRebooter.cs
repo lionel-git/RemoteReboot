03fd218167f3f5da88d557212e1de5ae4af46749
lionel@lionel-pc2 <lionel.desorme@free.fr>
2021-02-13 14:14:34 +0100

    Update

--- RemoteReboot/RemoteRebooter.cs
+++ RemoteReboot/RemoteRebooter.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,17 +19,53 @@ namespace RemoteReboot
 
         private int _port;
         private string _token;
+        private bool _dryRun = true;
+        private bool _forceAppsClosed = true;
 
         private volatile bool _running = true;
 
         public bool ConsoleMode { get; set; }
 
+        [DllImport("ShutdownWrapper.dll", CharSet = CharSet.Unicode)]
+        public static extern bool MySystemReboot(bool forceAppsClosed, bool dryRun);
+
+        public const string ResponseOk = "OK";
+        public const string ResponseNotOk = "NOT OK";
+
+        private byte[] GetResponse(string response)
+        {
+            _logger.Info($"Response: {response}");
+            return Encoding.ASCII.GetBytes(response);
+        }
+
         private void MainLoop()
         {
+            _logger.Info($"Start listening on port={_port}...");
+            var endPoint = new IPEndPoint(IPAddress.Any, _port);
+            var tcpListener = new TcpListener(endPoint);
+            tcpListener.Start();            
             while (_running)
             {
-               
-                Thread.Sleep(1000);
+                using (var socket = tcpListener.AcceptSocket())
+                {
+                    _logger.Info($" Received connection from {socket.RemoteEndPoint}");
+                    var buffer = new byte[1024];
+                    int nbBytes = socket.Receive(buffer);
+                    _logger.Info($"Received: {nbBytes} bytes");
+                    var token = Encoding.ASCII.GetString(buffer, 0, nbBytes);
+                    byte[] response;
+                    if (token == _token)
+                    {
+                        response = GetResponse(ResponseOk);
+                        MySystemReboot(_forceAppsClosed, _dryRun);
+                    }
+                    else
+                    {
+                        response = GetResponse(ResponseNotOk);
+                    }
+                    socket.Send(response);
+                    socket.Shutdown(SocketShutdown.Both);
+                }
             }
             _logger.Info("Exiting main loop");
         }
@@ -35,14 +74,18 @@ namespace RemoteReboot
         {
             _port = int.Parse(ConfigurationManager.AppSettings["ServicePort"]);
             _token = ConfigurationManager.AppSettings["Token"].ToString();
+            _dryRun = bool.Parse(ConfigurationManager.AppSettings["DryRun"]);
+            _forceAppsClosed = bool.Parse(ConfigurationManager.AppSettings["ForceAppsClosed"]);
             if (string.IsNullOrWhiteSpace(_token))
                 throw new Exception("Token is null or empty, set it to a value!!!");
+            _logger.Info($"Config: port={_port}, dryRun={_dryRun}, forceAppsClosed={_forceAppsClosed}");
         }
 
         public void OnStart(string[] args)
         {
             _logger.Info("=====================================================");
             InitConfig(args);
+            MainLoop();
         }
 
         public void OnStop()
--- RemoteRebootClient/Program.cs
+++ RemoteRebootClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,39 @@ namespace RemoteRebootClient
 {
     class Program
     {
+        static void GetGuid()
+        {
+            var guid = Guid.NewGuid();
+            Console.WriteLine(guid);
+        }
+
         static void Main(string[] args)
         {
+            try
+            {
+                GetGuid(); return;
+
+
+                   var hostname = ConfigurationManager.AppSettings["Hostname"].ToString();
+                var port = int.Parse(ConfigurationManager.AppSettings["ServicePort"]);
+                var token = ConfigurationManager.AppSettings["Token"].ToString();
+                if (string.IsNullOrWhiteSpace(token))
+                    throw new Exception("Token is null or empty, set it to a value!!!");
+
+                using (var client = new TcpClient())
+                {
+                    client.Connect(hostname, port);
+                    client.Client.Send(Encoding.ASCII.GetBytes(token));
+                    var buffer = new byte[1024];
+                    int r = client.Client.Receive(buffer);
+                    var ret = Encoding.ASCII.GetString(buffer, 0, r);
+                    Console.WriteLine($"Received: {ret}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
